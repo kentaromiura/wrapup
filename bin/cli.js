daa998f1853e0bdff18cc8cb4fde9a1f62692415
Carlesso Cristian @kentaromiura <kentaromiura@gmail.com>
2021-06-05 18:30:17 +0900

    Upgrade to latest commander

--- bin/cli.js
+++ bin/cli.js
@@ -1,20 +1,20 @@
 "use strict";
 
-var path     = require('path')
-var program  = require('commander')
-var chalk    = require('chalk')
+var path = require('path')
+var program = require('commander').program
+var chalk = require('chalk')
 var relative = require('require-relative').resolve
-var WrapUp   = require('../lib/wrapup')
-var json     = require('../package')
-var error    = require('./errorHandler')
+var WrapUp = require('../lib/wrapup')
+var json = require('../package')
+var error = require('./errorHandler')
 
 var wrapup = new WrapUp()
 
-wrapup.on('change', function(file){
+wrapup.on('change', function (file) {
     console.warn(chalk.blue.inverse("=>") + " " + chalk.grey(path.relative(process.cwd(), file)) + " was changed")
 })
 
-wrapup.scanner.on('warn', function(err){
+wrapup.scanner.on('warn', function (err) {
     error.errorHandler(err, true)
 })
 
@@ -26,17 +26,17 @@ program
     .option('-t, --transform', 'a module that can transform source code or ASTs')
     .option('--in-path <path>', 'all required files should be in this path')
 
-program.on('require', function(option){
+program.on('option:require', function (option) {
     var parts = option.split('=')
     if (parts.length > 1) wrapup.require(parts[0], parts[1])
     else wrapup.require(option)
 })
 
-program.on('transform', function(option){
+program.on('option:transform', function (option) {
     wrapup.scanner.addTransform(require(relative(option)))
 })
 
-program.on('in-path', function(option){
+program.on('option:in-path', function (option) {
     wrapup.scanner.set('inPath', program.inPath)
 })
 
@@ -46,7 +46,7 @@ require('./browser')(program, wrapup)
 require('./amdCombined')(program, wrapup)
 require('./amd')(program, wrapup)
 
-program.outputHelp = function(){
+program.outputHelp = function () {
     // header
     console.warn(chalk.grey(" , , , __  __.  _   . . _  "))
     console.warn(chalk.grey("(_(_/_/ (_(_/|_/_)_(_/_/_)_"))
